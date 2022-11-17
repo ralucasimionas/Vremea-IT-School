6ad8e1ef3228575b4597486aeb085b2288d875df
ralucasimionas <sim_ralu@yahoo.com>
2022-11-17 19:47:26 +0200

    Adaugat functionalitate de salvare locala

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-displayCurrentWeather("București");
+let currentCity = localStorage.getItem("city");
+
+if (!currentCity) {
+  currentCity = "București";
+  localStorage.setItem("city", "București");
+}
+updateCityDisplay(currentCity);
+
+displayCurrentWeather(currentCity);
--- sections/ChooseLocation.js
+++ sections/ChooseLocation.js
@@ -2,10 +2,16 @@ const bucharest = document.querySelector(".bucharest");
 const oradea = document.querySelector(".oradea");
 const timisoara = document.querySelector(".timisoara");
 
-function updateCity(city) {
-  // updatam orasul afisat
+function updateCityDisplay(city) {
   const currentCity = document.getElementById("current-city");
   currentCity.innerHTML = city;
+}
+
+function updateCity(city) {
+  // updatam orasul afisat
+  updateCityDisplay(city);
+  //   salvam noul oras in localStorage
+  localStorage.setItem("city", city);
 
   // afisam noile date de la API
   displayCurrentWeather(city);
